313a7fbcb162b9c71d25b103de5f00a35d439f36
abhilashgd <anrewgd@gmail.com>
2021-04-05 23:11:22 +0530

    Update Checkout.java

--- Shopping/Checkout.java
+++ Shopping/Checkout.java
@@ -1,6 +1,8 @@
-package java;
+package shopping;
 
 public interface CheckOut {
+    void SpecialOfferServiceInitializer(SpecialOffer soc);
     void scan(String str);
     int calculateMyOrderTotal();
 }
+
