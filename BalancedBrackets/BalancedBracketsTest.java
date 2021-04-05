80539438f2ee860e6c458afde41b86fa73f113d5
abhilashgd <anrewgd@gmail.com>
2021-04-05 21:06:38 +0530

    Update BalancedBracketsTest.java

--- BalancedBrackets/BalancedBracketsTest.java
+++ BalancedBrackets/BalancedBracketsTest.java
@@ -1,9 +1,10 @@
-package Tests;
+package BalancedBrackets;
 
-import Brackets.BalancedBrackets;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BalancedBracketsTest {
     BalancedBrackets bb = new BalancedBrackets();
@@ -11,29 +12,38 @@ public class BalancedBracketsTest {
     @Test
     public void isBalancedTest1(){
         String Test1 = "()()()()";
-        assertEquals("true",bb.isBalanced(Test1));
+        Assertions.assertEquals("true",bb.isBalanced(Test1));
 
     }
 
     @Test
     public void isBalancedTest2(){
         String Test2 ="[(])";
-        assertEquals("true",bb.isBalanced(Test2));
+        Assertions.assertEquals("true",bb.isBalanced(Test2));
 
     }
     @Test
     public void isBalancedTest3(){
 
         String Test3="(())){}[][][]";
-        assertEquals("false",bb.isBalanced(Test3));
+        Assertions.assertEquals("false",bb.isBalanced(Test3));
 
-}
+    }
 
     @Test
     public void isBalancedTest4(){
 
         String Test3="(()){}[][][]()()(){}{}{}{}{}[][][][][][]";
-        assertEquals("true",bb.isBalanced(Test3));
+        Assertions.assertEquals("true",bb.isBalanced(Test3));
 
     }
+
+    @Test
+    public void isBalancedTest5(){
+
+        String Test=")(";
+        Assertions.assertEquals("false",bb.isBalanced(Test));
+
+    }
+
 }
